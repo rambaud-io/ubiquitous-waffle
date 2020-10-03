ca5bbb92f72afd0600e2c3e642230154fb01c123
Darren Rambaud <xyzst@users.noreply.github.com>
2020-10-03 12:14:23 -0500

    Add health check API
    - to be used in conjunction with load balancing the app
    
    Related to: #3

--- cmd/init/server.go
+++ cmd/init/server.go
@@ -12,6 +12,11 @@ func main() {
 		fmt.Fprintf(w, "I am just here for the t-shirt")
 	})
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r * http.Request) {
+		// todo add request logging
+		fmt.Fprintf(w, "OK")
+	})
+
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		// todo add logging if failed
